17b7b6fdfb3cfd5fe692d636344e4507fec7f805
garimaart <cedcoss@garimaarora.com>
2021-08-14 19:36:10 +0530

    seeding and factory added

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\User::factory()->time(10)->create();
+       DB::table('users')->insert([
+        'name' => Str::random(10), 
+        'email' => Str::random(10).'@gmail.com',
+        'password' => Hash::make('password'),]);
+        User::factory()->count(50)->create();
+    }
+   
+        
+    }
